1172ca54994df0f8da9f1bda6b91610c5bc85173
thank267 <thank267@me.com>
2021-06-22 15:44:34 +0500

    lesson-5

--- MainClass.java
+++ MainClass.java
@@ -1,17 +1,31 @@
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        final CyclicBarrier preparationСyclicBarrier = new CyclicBarrier(CARS_COUNT,
+                () -> System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!")
+        );
+
+        final CyclicBarrier finishСyclicBarrier = new CyclicBarrier(CARS_COUNT, 
+                () -> System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!")
+        );
+
+        final Semaphore tunnelSemaphore = new Semaphore(CARS_COUNT/2);
+
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, preparationСyclicBarrier, tunnelSemaphore, finishСyclicBarrier, 20 + (int) (Math.random() * 10));
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        
+        
+        
     }
 }
